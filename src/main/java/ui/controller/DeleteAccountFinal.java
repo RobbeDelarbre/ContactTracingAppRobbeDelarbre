776a6bbcb737f0ddb82d30da9a31c1eacac7a497
RobbeDelarbre <robbe.delarbre@gmail.com>
2020-10-11 00:06:19 +0200

    Verwijder account werkt nu echt :P

--- src/main/java/ui/controller/DeleteAccountFinal.java
+++ src/main/java/ui/controller/DeleteAccountFinal.java
@@ -1,13 +1,18 @@
 package ui.controller;
 
+import domain.model.Person;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class DeleteAccountFinal extends RequestHandler{
 
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
         if (request.getParameter("ja") != null){
+            Person p = (Person) request.getSession(false).getAttribute("user");
+            service.delete(p.getUserid());
             request.getSession().invalidate();
             return "accountDeletedConfirmation.jsp";
         } else if (request.getParameter("nee") != null){
